b7b551a9eba17aa7e10412ad088da425c33f5a48
FabinhoEdinei <59501299+FabinhoEdinei@users.noreply.github.com>
2020-09-05 11:59:09 -0300

    teste nova zero party Mexendo um pouco com buttons

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
 import capaImage from "./Image/arrow-circle-right-solid.svg";
+
 export default function EstoquePlate() {
   return (
     <div className="basePlate">
@@ -9,9 +10,20 @@ export default function EstoquePlate() {
         <img src={capaImage} alt="seta" />
         for you !
       </p>
-      <p className="basePilhaUm">Edinei</p>
-      <p className="basePilhaDois">Ed</p>
+<ul>
+<li>
+
+      <p className="basePilhaUm">
+    Edinei</p>
       <p className="basePilhaTres">For You</p>
+      <p className="basePilhaDois">Ed
+
+  <button type="submit">click aqui...........</button>
+      </p>
+  
+   </li>
+<li>nome</li>
+</ul>
     </div>
   );
 }
